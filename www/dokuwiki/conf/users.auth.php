c30e42b478f1b5bcbe20ddeba55dfbc846d8ea75
James Cherven <jcherven@mail.usf.edu>
2018-09-01 11:13:43 -0400

    initial commit

---
+++ www/dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+jcherven:$1$TSdA9f4k$y89B9TfAeK.5n1uGYT..J.:James:jcherven@mail.usf.edu:admin,user
